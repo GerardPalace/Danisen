c59a4a0964ab00480ad195a2e11c6c1bae251394
GerardPalace <pimmel.colin59@gmail.com>
2018-02-09 23:25:14 +0100

    ranking

---
+++ html.py
@@ -0,0 +1,6 @@
+import sys
+import io
+
+c = ["fox", "falco", "marth", "sheik", "jigglypuff", "peach", "ice_climbers", "falcon", "pikachu", "samus", "dr_mario", "yoshi", "luigi", "ganondorf", "mario", "young_link", "donkey_kong", "link", "mr_game_and_watch", "roy", "mewtwo", "zelda", "ness", "bowser", "pichu", "kirby"]
+for v in c:
+    open("./" + v + ".html", "w", encoding='utf-8')
