ba5b67d11e8f7c1e9daa848dbbd46e325ac6ba67
GerardPalace <colin.pimmel@BU001-13.peda.pdb.univ-lille3.fr>
2018-02-14 15:05:52 +0100

    Debut des feuille de stats

--- Scripts/html.py
+++ Scripts/html.py
@@ -3,4 +3,4 @@ import io
 
 c = ["fox", "falco", "marth", "sheik", "jigglypuff", "peach", "ice_climbers", "falcon", "pikachu", "samus", "dr_mario", "yoshi", "luigi", "ganondorf", "mario", "young_link", "donkey_kong", "link", "mr_game_and_watch", "roy", "mewtwo", "zelda", "ness", "bowser", "pichu", "kirby"]
 for v in c:
-    open("./" + v + ".html", "w", encoding='utf-8')
+    open("../" + v + ".html", "w", encoding='utf-8')
